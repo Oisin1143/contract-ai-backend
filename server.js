338d35e6bc4794a024920a1f88500eb2feb03b32
Oisin1143 <oisin.m.coughlan@gmail.com>
2026-03-16 23:06:20 +0000

    Remove cheerio require

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors    = require("cors");
-const cheerio = require("cheerio");
 const rateLimit = require("express-rate-limit");
 
 
